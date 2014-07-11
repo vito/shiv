f0765b7462d3e029e25e87933a5c70fcca07c528
Alex Suraci <suraci.alex@gmail.com>
2014-07-10 19:17:43 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/pkg/term"
+
+	wclient "github.com/cloudfoundry-incubator/garden/client"
+	wconnection "github.com/cloudfoundry-incubator/garden/client/connection"
+	"github.com/cloudfoundry-incubator/garden/warden"
+)
+
+var wardenNetwork = flag.String(
+	"wardenNetwork",
+	"tcp",
+	"warden server network (e.g. unix, tcp)",
+)
+
+var wardenAddr = flag.String(
+	"wardenAddr",
+	"127.0.0.1:7777",
+	"warden server address",
+)
+
+func main() {
+	flag.Parse()
+
+	handle := flag.Arg(0)
+
+	term, err := term.Open(os.Stdin.Name())
+	if err != nil {
+		log.Fatalln("failed to open terminal:", err)
+	}
+
+	err = term.SetRaw()
+	if err != nil {
+		log.Fatalln("failed to thaw terminal:", err)
+	}
+
+	client := wclient.New(wconnection.New(*wardenNetwork, *wardenAddr))
+
+	var container warden.Container
+	if handle == "" {
+		container, err = client.Create(warden.ContainerSpec{
+			RootFSPath: "docker:///ubuntu#14.04",
+		})
+	} else {
+		container, err = client.Lookup(handle)
+	}
+
+	if err != nil {
+		term.Restore()
+		log.Fatalln("failed to lookup container:", err)
+	}
+
+	process, err := container.Run(warden.ProcessSpec{
+		Path: "bash",
+		Args: []string{"-l"},
+	}, warden.ProcessIO{
+		Stdin:  term,
+		Stdout: term,
+		Stderr: term,
+	})
+	if err != nil {
+		term.Restore()
+		log.Fatalln("failed to run:", err)
+	}
+
+	process.Wait()
+	term.Restore()
+}
